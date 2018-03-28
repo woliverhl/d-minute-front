b635c9e3e252e89e604e746bd11b2db8d14fde17
Felipe Sandoval <soul@Felipes-MacBook-Pro.local>
2018-03-28 18:56:49 -0300

    allowallowCrossDomain server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 
+app.use(allowCrossDomain);
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
