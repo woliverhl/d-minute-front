cb1caff7c92d35c6e2979a4d6cf7c1286042b212
Felipe Sandoval <soul@Felipes-MacBook-Pro.local>
2018-03-28 18:59:38 -0300

    allowallowCrossDomain server.js

--- server.js
+++ server.js
@@ -2,6 +2,20 @@
 const express = require('express');
 const app = express();
 
+const allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+
 app.use(allowCrossDomain);
 
 // Serve only the static files form the dist directory
