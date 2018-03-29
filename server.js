96231915885ac28dfd0565a79a5c66160843aa3b
Felipe Sandoval <soul@Felipes-MacBook-Pro.local>
2018-03-29 11:00:27 -0300

    se modifica server.js con origin undefined y cors

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 //Install express server
-const express = require('express');
-const app = express();
+const express = require('express'),
+    app = express(),
+    cors = require('cors');
+
+const configUndefined = function (req, res, next) {
+    if (!req.headers.origin) {
+        express.static(__dirname + '/dist')
+    } else {
+        next();
+    }
+}
+
+const configCors = {
+    // origin handler
+    origin: function (origin, cb) {
+        // setup a white list
+        let wl = ['https://dminutems.herokuapp.com'];
+        if (wl.indexOf(origin) != -1) {
+            cb(null, true);
+        } else {
+            cb(new Error('invalid origin: ' + origin), false);
+        }
+    },
+        optionsSuccessStatus: 200
+    }
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(configUndefined, cors(configCors));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
