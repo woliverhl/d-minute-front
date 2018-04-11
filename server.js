de1c8a4f0f2faa19a4b18e39c0f7766cc67c5278
woliverhl <ohidalgoleal@gmail.com>
2018-04-10 23:52:49 -0300

    config cors

--- server.js
+++ server.js
@@ -1,26 +1,79 @@
-const express = require('express');
-const app = express();
-const history = require('connect-history-api-fallback');
-
-const allowCrossDomain = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  // intercept OPTIONS method
-  if (req.method === 'OPTIONS') {
-    res.send(200);
-  } else {
-    next();
-  }
+let express = require('express'),
+cors = require('cors'),
+os = require('os'),
+app = express(),
+ 
+// hard coded configuration object
+conf = {
+    // look for PORT environment variable,
+    // else look for CLI argument,
+    // else use hard coded value for port 8080
+    port: process.env.PORT || process.argv[2] || 8080,
+ 
+    // origin undefined handler
+    // see https://github.com/expressjs/cors/issues/71
+    originUndefined: function (req, res, next) {
+ 
+        if (!req.headers.origin) {
+ 
+            res.json({
+ 
+                mess: 'Hi you are visiting the service locally. If this was a CORS the origin header should not be undefined'
+ 
+            });
+ 
+        } else {
+ 
+            next();
+ 
+        }
+ 
+    },
+ 
+    // Cross Origin Resource Sharing Options
+    cors: {
+ 
+        // origin handler
+        origin: function (origin, cb) {
+ 
+            // setup a white list
+            let wl = ['https://dminutems.herokuapp.com'];
+ 
+            if (wl.indexOf(origin) != -1) {
+ 
+                cb(null, true);
+ 
+            } else {
+ 
+                cb(new Error('invalid origin: ' + origin), false);
+ 
+            }
+ 
+        },
+ 
+        optionsSuccessStatus: 200
+ 
+    }
+ 
 };
-app.use(allowCrossDomain);
-app.use(history());
-app.options('*', cors());
-app.use(cors());
-app.use(express.static(__dirname + '/dist'));
-
-app.set('port', (process.env.PORT || 8080));
-
-app.listen(app.get('port'), () => {
-  console.log(`Server launched on ${process.env.port || 8080}`);
+ 
+// use origin undefined handler, then cors for all paths
+app.use(conf.originUndefined, cors(conf.cors));
+ 
+// get at root
+app.get('/', function (req, res, next) {
+ 
+    res.json({
+        mess: 'hello it looks like you are on the whitelist',
+        origin: req.headers.origin,
+        os_hostname: os.hostname(),
+        os_cpus: os.cpus()
+    });
+ 
+});
+ 
+app.listen(conf.port, function () {
+ 
+    console.log('CORS-enabled JSON service is live on port: ' + conf.port);
+ 
 });
