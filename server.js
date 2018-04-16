b22a79a9107c0ea44f786a6c727b5ea39976ccbe
woliverhl <ohidalgoleal@gmail.com>
2018-04-15 21:23:52 -0300

    cors server

--- server.js
+++ server.js
@@ -1,32 +1,5 @@
-var express = require('express');
+var express = require('express')
+  , cors = require('cors')
+  , app = express();
 
-var app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.get('/', function (req, res) {
-  var data = {
-    "bestAnimals": [
-      "wombat",
-      "corgi",
-      "puffer fish",
-      "owl",
-      "crow"
-    ]
-  };
-
-  res.json(data);
-});
-
-
-var server = app.listen(3000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
-});
+app.use(cors());
