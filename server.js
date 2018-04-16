26cbf4f80375637890a568c4bda2e84f33efd100
woliverhl <ohidalgoleal@gmail.com>
2018-04-15 21:47:15 -0300

    cors server ok

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ app.set('views', path.join(__dirname, 'src'));
 app.use(express.static(__dirname + '/dist'));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin: *');
+  res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
   next();
 });
 
