045f84d1d0a238fd90076d78f592a1f0e18f910a
woliverhl <ohidalgoleal@gmail.com>
2018-04-15 21:30:50 -0300

    cors server ok

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-var express = require('express')
-  , cors = require('cors')
-  , app = express();
+var express = require('express');
+var path = require('path');
+var cors = require('cors')
 
+var app = express();
 app.use(cors());
+app.set('views', path.join(__dirname, 'src'));
+app.use(express.static(__dirname + '/dist'));
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
