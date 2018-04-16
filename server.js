0d83174021cf0991bb30420aa7c1c465d715f15b
woliverhl <ohidalgoleal@gmail.com>
2018-04-15 21:37:11 -0300

    cors server ok

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ var path = require('path');
 var cors = require('cors')
 
 var app = express();
-app.use(cors());
 app.set('views', path.join(__dirname, 'src'));
 app.use(express.static(__dirname + '/dist'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
