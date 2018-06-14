7b1cbeb1caa3b2604015d7309c38e2a782d0fc72
woliverhl <ohidalgoleal@gmail.com>
2018-06-14 00:33:37 -0400

    comentar api

--- src/app/share/constants/restPath.ts
+++ src/app/share/constants/restPath.ts
@@ -1,6 +1,6 @@
 export class restPath {
-    public APP: string = '/api';
-    //public APP: string = 'https://dminutems.herokuapp.com';
+    //public APP: string = '/api';
+    public APP: string = 'https://dminutems.herokuapp.com';
     public logIn: string = '/token/generate-token';
     public listProyects = '/proyecto/listarProyectoUsuario';
     public listUsers = '/usuario/listaUsuarios';
