dfa27dc751ea3473aea6f8aa6c89b3fd8613375b
Oliver Hidalgo <ohidalgoleal@gmail.com>
2018-06-18 16:17:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('views', path.join(__dirname, 'src'));
 app.use(express.static(__dirname + '/dist'));
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://dminutems.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
