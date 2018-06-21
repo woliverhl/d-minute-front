92d03e64e7bb83b267003bb3bb0a40c2f9281327
woliverhl <ohidalgoleal@gmail.com>
2018-06-21 14:30:19 -0400

    proxy server

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var path = require('path');
-var cors = require('cors')
-var http = require('http'),
-    httpProxy = require('http-proxy');
 
 var app = express();
 app.set('views', path.join(__dirname, 'src'));
 app.use(express.static(__dirname + '/dist'));
-
-var proxy = httpProxy.createProxyServer({});    
-httpProxy.createProxyServer({
-    target:'http://dminutezuul.herokuapp.com'  
-  }).listen(process.env.PORT);
 
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
