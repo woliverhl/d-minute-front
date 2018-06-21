cae878adfefbd4c56d37b54e8aa336c9a1ee9e3e
woliverhl <ohidalgoleal@gmail.com>
2018-06-21 13:55:12 -0400

    proxy server

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var path = require('path');
 var cors = require('cors')
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+var proxy = httpProxy.createProxyServer({});    
+httpProxy.createProxyServer({
+    target:'http://dminutezuul.herokuapp.com'  
+  }).listen(process.env.PORT);
 
 var app = express();
 app.set('views', path.join(__dirname, 'src'));
 app.use(express.static(__dirname + '/dist'));
-
-var corsOptions = {
-  origin: 'http://dminutezuul.herokuapp.com',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-app.use(cors(corsOptions))
 
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
