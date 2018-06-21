f94b45f47ec4fcc53d80c0a12dc94720c7df3913
woliverhl <ohidalgoleal@gmail.com>
2018-06-21 15:24:47 -0400

    proxy server

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
-var express = require('express');
-var path = require('path');
-var http = require('http'),
-    httpProxy = require('http-proxy');
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const httpProxy = require('http-proxy');
 
-var app = express();
-app.set('views', path.join(__dirname, 'src'));
+//Creando el servidor
+const app = express();
 app.use(express.static(__dirname + '/dist'));
+app.set('views', path.join(__dirname, 'src'));
 
-//
+//Define puerto
+const port = process.env.PORT || '8080';
+app.set(port);
+
 // Create a proxy server with custom application logic
-//
-var proxy = httpProxy.createProxyServer({});
-//
-// Create your custom server and just call `proxy.web()` to proxy
-// a web request to the target passed in the options
-// also you can use `proxy.ws()` to proxy a websockets request
-//
-var server = http.createServer(function(req, res) {
+const proxy = httpProxy.createProxyServer({});
+
+const server = http.createServer(function(req, res) {
   // You can define here your custom logic to handle the request
   // and then proxy the request.
   proxy.web(req, res, { target: 'http://dminutezuul.herokuapp.com' });
-  app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
 });
