0b7ec4c45db85cb0eafe079e8947f9b171e8c49a
woliverhl <ohidalgoleal@gmail.com>
2018-06-21 16:02:13 -0400

    proxy server

--- server.js
+++ server.js
@@ -16,8 +16,11 @@ app.set(port);
 console.log("LOG: puerto seteado");
 
 // Create a proxy server with custom application logic
+httpProxy.createProxyServer({target:'http://dminutezuul.herokuapp.com'});
+console.log("LOG: proxy creado");
 //httpProxy.createProxyServer({target:'http://dminutezuul.herokuapp.com'}).listen(port);
 
 const server = http.createServer(app);
 console.log("LOG: server creado");
 app.listen(port, () => console.log("App now running on port", port));
+httpProxy.listen(port);
