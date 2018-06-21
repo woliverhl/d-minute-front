72d503fae20d215f506a04a3930e51963b09a54c
woliverhl <ohidalgoleal@gmail.com>
2018-06-21 17:05:03 -0400

    proxy server

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const httpProxy = require('http-proxy');
+const cors = require('cors');
 
 //Creando el servidor
 const app = express();
 app.use(express.static(__dirname + '/dist'));
 app.set('views', path.join(__dirname, 'src'));
+app.use(cors());
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 console.log("LOG: app creado");
 
