f6e1b2f9cac0e7c12a5eafbe0629997c08f40cd9
woliverhl <ohidalgoleal@gmail.com>
2018-06-21 12:24:37 -0400

    procfile

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'src'));
 app.use(express.static(__dirname + '/dist'));
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+var corsOptions = {
+  origin: 'http://dminutezuul.herokuapp.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
 
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
