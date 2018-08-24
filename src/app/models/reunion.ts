5f02ebc90e7a6adbc6cbd03a49ea74ab5af76e2b
oliver hidalgo <ohidalgoleal@gmail.com>
2018-08-24 15:05:30 -0300

    se sube propiedades pendientes

--- src/app/models/reunion.ts
+++ src/app/models/reunion.ts
@@ -1,14 +1,18 @@
 import { TemaActa } from "./tema";
 import { UsuarioActa } from "./usuarioActa";
+import { ElementoDialogo } from "./ElementoDialogo";
 
 export class Reunion {
     constructor(){}
     public fecha: Date;
     public resumen: String;
+    public horaInicio: String;
+    public horaFin: String;
     public correlativo: Number;
     public estado: String;
     public proyectoId : Number;
     public usuarioActa : UsuarioActa[];
     public temaActa: TemaActa[];
+    public tareaPendiente: ElementoDialogo[];
     public actaId: Number;
 }
