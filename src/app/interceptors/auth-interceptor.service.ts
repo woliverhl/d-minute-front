6b000ef1b3afc1be3811190369b09d00c22ee7c1
BCI-Macbook-Pro <ohidalgoleal@bci.cl>
2018-12-12 08:01:06 -0300

    versión 1.1 fixture login google

--- src/app/interceptors/auth-interceptor.service.ts
+++ src/app/interceptors/auth-interceptor.service.ts
@@ -1,9 +1,8 @@
-import { Injectable, ViewContainerRef, Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler , HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
 import { SessionService } from "app/session/service/session.service";
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/do';
-import { HttpObserve } from '@angular/common/http/src/client';
 import { HttpResponse } from '@angular/common/http';
 import { Router } from "@angular/router";
 import { SpinnerService } from '../share/spinner/spinner.service';
@@ -21,15 +20,17 @@ export class AuthInterceptorService implements HttpInterceptor {
 
   goodByeDude():void{
     this.auth.resetToken();
+    this.auth.resetOrigenToken();
     this.route.navigate(['/sign-on'])
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler):Observable<HttpEvent<any>> {
-    if (!request.url.match('/token/generate-token') && request['responseType'] === 'json'){
+    if (!request.url.match('/token/generate-token') && !request.url.match('/token/userOauth') && request['responseType'] === 'json'){
       this.auth.getToken() === undefined || this.auth.getToken() === "undefined"  ? this.goodByeDude() : undefined;
       request = request.clone({
         setHeaders: {
-          Authorization: `Bearer ${this.auth.getToken()}`
+          Authorization: `Bearer ${this.auth.getToken()}`,
+          Origen: `${this.auth.getOrigenToken()}`
         }
     });
     }
