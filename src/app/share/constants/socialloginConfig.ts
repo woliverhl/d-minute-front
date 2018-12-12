590b406bbfe521678ea5a5f27afb0a271f557a3d
BCI-Macbook-Pro <ohidalgoleal@bci.cl>
2018-12-12 09:00:29 -0300

    fix certificado d-minute google

--- src/app/share/constants/socialloginConfig.ts
+++ src/app/share/constants/socialloginConfig.ts
@@ -4,9 +4,9 @@ export function getAuthServiceConfigs() {
     let config = new AuthServiceConfig([{
         id: GoogleLoginProvider.PROVIDER_ID,
         //PROD
-        //provider: new GoogleLoginProvider("221017604524-ee6c3vvjhu33qskev7devgrl90sfpmuv.apps.googleusercontent.com")
+        provider: new GoogleLoginProvider("221017604524-qf91uilhvipme72fmesj4ag0fuegi3nd.apps.googleusercontent.com")
         //Local
-        provider: new GoogleLoginProvider("1078712008081-tgdr6je72gotima3c66uli90he8g5lmg.apps.googleusercontent.com")
+        //provider: new GoogleLoginProvider("1078712008081-tgdr6je72gotima3c66uli90he8g5lmg.apps.googleusercontent.com")
     }]);
     
     return config;
